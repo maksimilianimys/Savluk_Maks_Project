dba1ce84b2d6a8ce6d3ccc7a7d7fd2cdefeff9d4
Maks <maswety8@gmail.com>
2025-01-14 16:44:51 +0200

    create task2

---
+++ docs/js/burger_menu.js
@@ -0,0 +1,1 @@
+const hamMenu=document.querySelector(".ham-menu"),offScreenMenu=document.querySelector(".menu-mobile");hamMenu.addEventListener("click",(()=>{hamMenu.classList.toggle("active"),offScreenMenu.classList.toggle("active")}));
